08920ca8d596a2866790ed5a866a6429cf644f5f
madhusudhanansuresh <33403157+madhusudhanansuresh@users.noreply.github.com>
2018-11-01 17:18:30 +0530

    initial commit

---
+++ 1EditProgram/1EditProgram/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstString = "Madhus";
+            string secondString = "Madhu";
+            if (firstString.Length == secondString.Length)
+            {
+                Console.WriteLine(findIfReplace(firstString, secondString));
+            }
+            else if (firstString.Length - 1 == secondString.Length)
+            {
+                Console.WriteLine(findIfInsert(firstString, secondString));
+
+            }
+            else if (firstString.Length == secondString.Length - 1)
+            {
+                Console.WriteLine(findIfSecond(secondString, firstString));
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+            Console.ReadLine();
+        }
+        private static bool findIfInsert(string firstString, string secondString)
+        {
+            int index1 = 0;
+            int index2 = 0;
+
+            bool setFalseFlag = false;
+
+            while (index1 < firstString.Length && index2 < secondString.Length)
+            {
+                if (firstString[index1] != secondString[index2])
+                {
+                    if (setFalseFlag)
+                    {
+                        index1++;
+                        return false;
+                    }
+                    setFalseFlag = true;
+
+                }
+                index1++;
+                index2++;
+            }
+            return true;
+
+        }
+
+        private static bool findIfSecond(string firstString, string secondString)
+        {
+            int index1 = 0;
+            int index2 = 0;
+
+            bool setFalseFlag = false;
+
+            while (index1 < firstString.Length && index2 < secondString.Length)
+            {
+                if (firstString[index1] != secondString[index2])
+                {
+                    if (setFalseFlag)
+                    {
+                        index1++;
+                        return false;
+                    }
+                    setFalseFlag = true;
+
+                }
+                index1++;
+                index2++;
+            }
+            return true;
+        }
+
+        private static bool findIfReplace(string firstString, string secondString)
+        {
+            bool setfalseFlag = false;
+            for (int i = 0; i < firstString.Length; i++)
+            {
+                if (firstString[i] != secondString[i])
+                {
+                    if (setfalseFlag)
+                    {
+                        return false;
+                    }
+
+                    setfalseFlag = true;
+                }
+            }
+            return true;
+        }
+    }
+}
